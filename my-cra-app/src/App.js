578080040e14fbdb56500fc5dcdc72133939ac61
Prerna Gupta <prerna.gupta@nimapinfotech.com>
2025-09-11 12:30:15 +0530

    added cra project

---
+++ my-cra-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome, React Project</h1>
+      <p>This is my first react project.</p>
+    </div>
+  );
+}
+
+export default App;
