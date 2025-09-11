99fbadd4d1ee3524c60ada7b43e75f1fc29bee4e
Prerna Gupta <prerna.gupta@nimapinfotech.com>
2025-09-11 12:13:03 +0530

    create a project

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Welcome, React Project</h1>
+      <p>This is my first react project.</p>
+    </div>
+  )
+}
+
+export default App
